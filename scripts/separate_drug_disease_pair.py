9a90161370d226e3ad925d87be066c436b0b23c7
James Chung <jchung@renci.org>
2024-07-12 17:02:30 -0400

    sep script update

--- scripts/separate_drug_disease_pair.py
+++ scripts/separate_drug_disease_pair.py
@@ -1,5 +1,6 @@
 import json
 import tqdm
+import os
 
 with open("/home/Embeddings/data/drug_disease/disease_ids.json", "r", encoding='utf-8-sig') as disf:
     disj = json.load(disf)
@@ -8,13 +9,17 @@ with open("/home/Embeddings/data/drug_disease/disease_ids.json", "r", encoding='
 with open("/home/Embeddings/data/drug_disease/drug_ids.json", "r", encoding='utf-8-sig') as drugf:
     drugj = json.load(drugf)
     drugset= set(drugj['drug_ids'])
-    
-trainedgef = open("/home/Embeddings/data/rkg_embedding_input/train_edges.jsonl", "w")
-removededgef2 = open("/home/Embeddings/data/rkg_embedding_input/removed_edges_rev.jsonl", "w")
-removededgef = open("/home/Embeddings/data/rkg_embedding_input/removed_edges.jsonl", "w")
+
+ROOTPATH = "/home/jchung/ROBOKOP/git/xDTD_training_pipeline"
+rkgf = os.path.join(ROOTPATH, "data/rkg_embedding_input")
+if not os.path.exists(rkgf): 
+    os.makedirs(rkgf)
+trainedgef = open(os.path.join(rkgf, "train_edges.jsonl"), "w")
+removededgef2 = open(os.path.join(rkgf, "removed_edges_rev.jsonl"), "w")
+removededgef = open(os.path.join(rkgf, "removed_edges.jsonl"), "w")
 
 
-with open("/home/Embeddings/data/edges.jsonl") as edgef:
+with open(os.path.join(ROOTPATH, "data/edges.jsonl")) as edgef:
     for i, l in enumerate(tqdm.tqdm(edgef)):
         j = json.loads(l)
         if (j['subject'] in drugset and j['object'] in disset):

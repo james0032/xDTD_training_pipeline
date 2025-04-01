350b69a360ec047e38bfb7bfb2c6d337dae26beb
james0032 <jchung@renci.org>
2025-04-01 13:01:27 -0400

    NOTE graphsage patch edited, restored original before this commit if needed

--- graphsage/utils.py
+++ graphsage/utils.py
@@ -68,7 +68,7 @@ def load_data(prefix, rw, normalize=True, load_walks=False):
         feats = scaler.transform(feats)
     
     if load_walks:
-        with open(prefix + "-walks{rw:s}.txt".format(rw)) as fp:
+        with open(prefix + "-walks{rw:s}.txt".format(rw=rw)) as fp:
             for line in fp:
                 walks.append(map(conversion, line.split()))
 

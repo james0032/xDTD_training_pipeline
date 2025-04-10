8745c337d6160eeeecb3b14fb63fa69cac98b02a
james0032 <jchung@renci.org>
2025-04-10 00:02:12 -0400

    sample size specific folder creation

--- graphsage/unsupervised_train.py
+++ graphsage/unsupervised_train.py
@@ -61,13 +61,13 @@ GPU_MEM_FRACTION = 0.8
 
 def log_dir():
     log_dir = FLAGS.base_log_dir + "/unsup-" + FLAGS.train_prefix.split("/")[-2]
-    log_dir += "/{model:s}_{model_size:s}_{lr:0.6f}_{rw:s}_{samples_1:d}_{samples_2:d}/".format(
+    log_dir += "/{model:s}_{model_size:s}_{lr:0.6f}_{s1:s}_{s2:s}/".format(
             model=FLAGS.model,
             model_size=FLAGS.model_size,
             lr=FLAGS.learning_rate,
-            rw=FLAGS.random_walk_version,
-            samples_1=FLAGS.samples_1,
-            samples_2=FLAGS.samples_2
+            #rw=FLAGS.random_walk_version,
+            s1=FLAGS.samples_1,
+            s2=FLAGS.samples_2
             )
     if not os.path.exists(log_dir):
         os.makedirs(log_dir)
